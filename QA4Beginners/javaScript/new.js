ee12555bd7c0ad9455cc9f3e15b56302379c879c
DeangeloW <williamsdeangelo4@gmail.com>
2022-07-07 12:25:58 -0600

    Add files via upload

---
+++ QA4Beginners/javaScript/new.js
@@ -0,0 +1,40 @@
+//Data Types
+
+//Strings: Example: "Hello", "i", "3"
+
+//Numbers: Example: 1, 100, 0.5
+
+//Bool: true, false
+
+//x + 1 = 3
+//where x = 2
+
+/*let x
+x = 1
+var isItTrue = true
+isItTrue
+let word = "Hello World"
+console.log(word)
+console.log(isItTrue) */
+
+// If statements
+if (1 + 1 == 2){
+    console.log("That is correct")
+
+}
+
+// While loops
+while (i < 100){
+    // do something
+}
+
+// For loops
+for (var i = 0; i < 10; i++){
+    // do something
+}
+
+// Functions
+
+function sayHello (word) {
+    console.log(word);
+}
